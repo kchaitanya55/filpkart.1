d117580d0094704d340b3b07cce7ea1b3bbb78a3
root <root@ip-172-31-33-138.eu-north-1.compute.internal>
2025-06-23 18:29:52 +0000

    modified login1.java

---
+++ login1.java
@@ -0,0 +1,12 @@
+[200~public class CloudinaryApiApplication {
+
+		public static void main(String[] args) {
+					SpringApplication.run(CloudinaryApiApplication.class, args);
+						}
+			@Bean
+				public RestTemplate getRestTemplate() {
+							return new RestTemplate();
+								}
+
+}
+
